82832a6c923df57470b7b1087750c9599ca35843
Joshua Newton <joshuacwnewton@gmail.com>
2021-05-09 14:23:13 -0400

    Clean up unnecessary use of apply and etc.
    Embarrassed that people are looking at my code. :sweat:

--- submit-listens.py
+++ submit-listens.py
@@ -4,26 +4,30 @@ import pylistenbrainz
 from tqdm import tqdm
 
 
-def convert_to_unix(series):
-    if series.name == "DateTime":
-        series = pd.to_datetime(series)
-        series = (series - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
+def convert_to_unix(date_series):
+    """Convert a series of DateTime-compatible values to UNIX time."""
+    date_series = pd.to_datetime(date_series)
+    date_series = (date_series - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
 
-    return series
+    return date_series
 
 
-def append_listen(series):
-    if series["DateTime"] != '':
+def append_listen(row):
+    """Convert each row into a pylistenbrainz.Listen and append to an external list."""
+    # This awkwardly relies on a `listens` list to exist outside the function
+    # There's probably a more elegant, side-effect free way of doing this
+    # But I'm just hacking things together for now
+    if row["DateTime"] != '':
         listens.append(
             pylistenbrainz.Listen(
-                track_name=series["track name"],
-                artist_name=series["artist name"],
-                release_name=series["album name"],
-                listened_at=int(series["DateTime"])
+                track_name=row["track name"],
+                artist_name=row["artist name"],
+                release_name=row["album name"],
+                listened_at=int(row["DateTime"])
             )
         )
 
-    return series  # Don't change the actual dataframe
+    return row  # Don't change the actual dataframe
 
 
 def chunks(lst, n):
@@ -36,7 +40,7 @@ path_csv_2009_2020 = "listens/scrobbles-merged-2009-2020.csv"
 df_listens = pd.read_csv(path_csv_2009_2020)
 
 print("Converting DateTime to UNIX time...")
-df_listens = df_listens.apply(convert_to_unix)
+df_listens["DateTime"] = convert_to_unix(df_listens["DateTime"])
 
 print("Replacing NANs...")
 df_listens = df_listens.replace(np.nan, '', regex=True)
